d2b567101c2265798390fae429e69b2c27925114
Satyaki Sen S <satyaki.s.sen@ericsson.com>
2020-03-10 21:10:01 +0530

    Adding fo routines and channels

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,17 +15,28 @@ func main() {
 		"http://www.facebook.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLinkStatus(link)
+		go checkLinkStatus(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			go checkLinkStatus(link, c)
+		}(l)
 	}
 }
 
-func checkLinkStatus(link string) {
+func checkLinkStatus(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "may be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up and running!")
+	c <- link
 }
